f6c659f5593ed1e0327474a6f7c292759694448f
oscarlove666 <939178127@qq.com>
2016-05-12 17:49:35 +0800

    5.12 公司

---
+++ game/dota_addons/test3/scripts/vscripts/creature_ability/animate_dead.lua
@@ -0,0 +1,18 @@
+function AnimateDead( event )
+	local caster = event.caster
+	local ability = event.ability
+	local team = event.caster:GetTeamNumber()
+	local duration = ability:GetLevelSpecialValueFor( "duration", ability:GetLevel() - 1 )
+	local max_units_resurrected = ability:GetLevelSpecialValueFor( "max_units_resurrected", ability:GetLevel() - 1 )
+
+	for i=1,max_units_resurrected do
+			local caster_location=caster:GetAbsOrigin()
+            local resurected_vector=Vector(caster_location.x+RandomInt(-radius, radius),caster_location.y+RandomInt(-radius, radius),0)
+            resurected_vector.z= GetGroundHeight(resurected_vector,nil)
+			local resurected = CreateUnitByName("npc_dota_abomination", resurected_vector, true, nil, nil, team)
+			-- Apply modifiers for the summon properties
+			resurected:AddNewModifier(caster, ability, "modifier_kill", {duration = duration})
+			ability:ApplyDataDrivenModifier(caster, resurected, "modifier_animate_dead", nil)
+
+	end
+end
