e1f7db876bbfec9702e6824c394764f66d2b6b1e
oscar666 <939178127@qq.com>
2017-01-03 23:52:15 +0800

    1.3

---
+++ game/dota_addons/test3/scripts/vscripts/heroes/hero_techies/suicide.lua
@@ -0,0 +1,9 @@
+function Suicide( keys )
+	local caster = keys.caster
+	local caster_location = caster:GetAbsOrigin()
+	local ability = keys.ability
+	local ability_level = ability:GetLevel() - 1
+
+	--Kill the caster
+	caster:Kill(ability, caster)
+end
