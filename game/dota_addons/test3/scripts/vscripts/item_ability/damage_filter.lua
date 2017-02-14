cb11d1aac5ee75aee5af686499b8ef7034cc5a70
oscar666 <939178127@qq.com>
2017-02-14 23:24:11 +0800

    2.14

--- game/dota_addons/test3/scripts/vscripts/item_ability/damage_filter.lua
+++ game/dota_addons/test3/scripts/vscripts/item_ability/damage_filter.lua
@@ -61,6 +61,21 @@ function CHoldoutGameMode:DamageFilter(damageTable)
                damage_table.damage_flags = DOTA_DAMAGE_FLAG_NONE
                ApplyDamage(damage_table)
           end
+          if victim and victim:HasModifier("modifier_share_damage_passive") then  --处理伤害共享技能
+              local allies = FindUnitsInRadius(DOTA_TEAM_BADGUYS, Vector(0,0,0), nil, -1, DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_ALL, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, 0, false )
+              for _,ally in pairs(allies) do  --共享伤害 不再伤害本体 
+                  if ally:HasAbility("share_damage_passive") and ally~=victim  then             
+                    local damage_table = {}
+                    damage_table.attacker = victim
+                    damage_table.victim = ally
+                    damage_table.damage_type = DAMAGE_TYPE_PURE
+                    damage_table.ability = victim:FindAbilityByName("share_damage_passive")
+                    damage_table.damage = damageTable.damage
+                    damage_table.damage_flags = DOTA_DAMAGE_FLAG_NONE
+                    ApplyDamage(damage_table)
+                  end
+              end 
+          end
     end
     if attacker:GetTeam()==DOTA_TEAM_BADGUYS then
         if damageTable.damagetype_const ~=1 and attacker.damageMultiple~=nil then  --Ability damage
