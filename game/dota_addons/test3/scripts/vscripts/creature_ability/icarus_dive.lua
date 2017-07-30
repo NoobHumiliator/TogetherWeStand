1f4b4afc1d053a372d4cf9224d7e6a7769a75e2e
oscar666 <939178127@qq.com>
2017-07-30 23:43:36 +0800

    7.30 台式

---
+++ game/dota_addons/test3/scripts/vscripts/creature_ability/icarus_dive.lua
@@ -0,0 +1,67 @@
+--[[
+	Author: Ractidous
+	Date: 28.01.2015.
+	Cast Icarus Dive.
+]]
+function CastIcarusDive( event )
+	local caster		= event.caster
+	local ability		= event.ability
+
+	local dashLength	= event.dash_length
+	local dashWidth		= event.dash_width
+	local dashDuration	= event.dash_duration
+
+	local modifierCasterName	= event.modifier_caster_name
+
+	local casterOrigin	= caster:GetAbsOrigin()
+	local casterAngles	= caster:GetAngles()
+	local forwardDir	= caster:GetForwardVector()
+	local rightDir		= caster:GetRightVector()
+
+	local ellipseCenter	= casterOrigin + forwardDir * ( dashLength / 2 )
+
+	local startTime = GameRules:GetGameTime()
+
+	caster:SetContextThink( DoUniqueString("updateIcarusDive"), function ( )
+
+		local elapsedTime = GameRules:GetGameTime() - startTime
+		local progress = elapsedTime / dashDuration
+
+		-- Interrupted
+		if not caster:HasModifier( modifierCasterName ) then
+			return nil
+		end
+
+		-- Calculate potision
+		local theta = -2 * math.pi * progress
+		local x =  math.sin( theta ) * dashWidth * 0.5
+		local y = -math.cos( theta ) * dashLength * 0.5
+
+		local pos = ellipseCenter + rightDir * x + forwardDir * y
+		local yaw = casterAngles.y + 90 + progress * -360  
+
+		pos = GetGroundPosition( pos, caster )
+		caster:SetAbsOrigin( pos )
+		caster:SetAngles( casterAngles.x, yaw, casterAngles.z )
+
+		return 0.03
+	end, 0 )
+
+end
+
+
+--[[
+	Author: Ractidous
+	Date: 28.01.2015.
+	Apply the burn modifier if the target doesn't have that.
+]]
+function CheckToBurn( event )
+	local caster		= event.caster
+	local target		= event.target
+	local ability		= event.ability
+	local modifierName	= event.modifier_burn_name
+
+	if not target:HasModifier( modifierName ) then
+		ability:ApplyDataDrivenModifier( caster, target, modifierName, {} )
+	end
+end
--- game/dota_addons/test3/scripts/vscripts/creature_ability/supernova.lua
+++ game/dota_addons/test3/scripts/vscripts/creature_ability/supernova.lua
@@ -14,6 +14,7 @@ end
 function OnDestroyEgg( keys )  --取消血线，移除BUFF
 	local target=keys.target
 	local caster=keys.caster
+	local ability=keys.ability
 
 	caster:RemoveModifierByNameAndCaster("modifier_supernova_buff_invulnerable", target)
     ParticleManager:DestroyParticle(target.bloodLinkParticle,false)
@@ -21,10 +22,14 @@ function OnDestroyEgg( keys )  --取消血线，移除BUFF
 
     local units = FindUnitsInRadius(DOTA_TEAM_GOODGUYS, Vector( 0, 0, 0 ) , nil, -1, DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_ALL, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, 0, false)
     for _,unit in pairs(units) do
-         if not unit:HasModifier("modifier_supernova_egg_die_dot") then
-            ability:ApplyDataDrivenModifier(target, unit, modifier_name, {Duration = 15})  --蛋蛋爆炸,全团上Debuff
+         if not unit:HasModifier("modifier_supernova_egg_die_dot") then --蛋蛋爆炸,全团上Debuff
+            ability:ApplyDataDrivenModifier(target, unit, "modifier_supernova_egg_die_dot", {Duration = 15}) 
+            unit:SetModifierStackCount("modifier_charge_dot",ability,1)
          else
-
+            local stack_count = target:GetModifierStackCount("modifier_supernova_egg_die_dot",ability)
+            unit:RemoveModifierByName("modifier_supernova_egg_die_dot")
+            ability:ApplyDataDrivenModifier(target, unit, "modifier_supernova_egg_die_dot", {Duration = 15}) 
+            unit:SetModifierStackCount("modifier_charge_dot",ability,stack_count+1)
          end
     end
 
@@ -33,25 +38,21 @@ function OnDestroyEgg( keys )  --取消血线，移除BUFF
 end
 
 
-function DotDamage( keys )  --取消血线，移除BUFF
-	local target = 
-	 if not target:HasModifier("modifier_charge_dot") then
-	        AddModifier(caster, target, ability, "modifier_charge_dot", nil)
-	        target:SetModifierStackCount("modifier_charge_dot",keys.ability,1)
-	         local particle= ParticleManager:CreateParticle("particles/units/heroes/hero_batrider/batrider_stickynapalm_stack.vpcf",PATTACH_OVERHEAD_FOLLOW,target)
-	               ParticleManager:SetParticleControl(particle, 1, Vector(0,1,0))
-	               target.charge_dot_particle= particle                                                                          
-	      else
-	       -- 获取玩家的Modifier叠加数
-	      local bullets_count = target:GetModifierStackCount("modifier_charge_dot",keys.ability)
-	      --print("stack_count"..bullets_count)
-	      bullets_count=bullets_count+1
-	      target:RemoveModifierByName("modifier_charge_dot")
-	      AddModifier(caster, target, ability, "modifier_charge_dot", nil)
-	      target:SetModifierStackCount("modifier_charge_dot",keys.ability,bullets_count)
-	      local particle= ParticleManager:CreateParticle("particles/units/heroes/hero_batrider/batrider_stickynapalm_stack.vpcf",PATTACH_OVERHEAD_FOLLOW,target)
-	      ParticleManager:SetParticleControl(particle, 1, Vector(0,bullets_count,0)) 
-	      target.charge_dot_particle= particle
-	      print("stack_count new"..target:GetModifierStackCount("modifier_charge_dot",keys.ability))                                                
-	 end
+function DotDamage( keys )  --造成伤害
+	local target = keys.target
+	local caster = keys.caster
+	local ability = keys.ability
+	
+	local stack_count = target:GetModifierStackCount("modifier_supernova_egg_die_dot",ability)
+    local basic_damage = ability:GetLevelSpecialValueFor( "dot_basic_damage" , ability:GetLevel() - 1 )
+
+    local damageTable = {
+                        victim=unit,    --受到伤害的单位
+                        attacker = caster,          --造成伤害的单位
+                        damage = math.pow(basic_damage, stack_count),
+                        damage_type = DAMAGE_TYPE_PURE,
+                        damage_flags = DOTA_DAMAGE_FLAG_HPLOSS
+                        }
+    ApplyDamage(damageTable)    --造成伤害
+
 end
