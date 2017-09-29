b0a73ec0f0e26ddd144f75c363331b13d0be91a8
oscarlove666 <939178127@qq.com>
2017-09-29 20:05:07 +0800

    9.29 公司

---
+++ game/dota_addons/test3/scripts/vscripts/creature_ai/werewolf_ai.lua
@@ -0,0 +1,104 @@
+--[[
+razor AI
+]]
+require( "ai_core" )
+
+behaviorSystem = {} -- create the global so we can assign to it
+
+function Spawn( entityKeyValues )
+	if  thisEntity:GetTeam()==DOTA_TEAM_BADGUYS then
+		thisEntity:SetContextThink( "AIThink", AIThink, 0.25 )
+		--狼嚎技能
+		howlAbility = thisEntity:FindAbilityByName( "werewolf_howl" )
+	    behaviorSystem = AICore:CreateBehaviorSystem( { BehaviorNone, BehaviorPlasma_Field} ) 
+    end
+end
+
+function AIThink() -- For some reason AddThinkToEnt doesn't accept member functions
+       return behaviorSystem:Think()
+end
+
+function CollectRetreatMarkers()
+	local result = {}
+	local i = 1
+	local wp = nil
+	while true do
+		wp = Entities:FindByName( nil, string.format("waypoint_%d", i ) )
+		if not wp then
+			return result
+		end
+		table.insert( result, wp:GetOrigin() )
+		i = i + 1
+	end
+end
+POSITIONS_retreat = CollectRetreatMarkers()
+
+--------------------------------------------------------------------------------------------------------
+BehaviorNone = {}
+function BehaviorNone:Evaluate()
+	return 2 -- must return a value > 0, so we have a default ¿ØÖÆ´ó·½Ïò£¬ÍùÒ»¸ö×î½üµÄÓ¢ÐÛ´¦¿¿½ü
+end
+
+function BehaviorNone:Begin()
+	self.endTime = GameRules:GetGameTime() + 1
+	self.target=nil
+	local allEnemies = FindUnitsInRadius( DOTA_TEAM_BADGUYS, thisEntity:GetOrigin(), nil, -1, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, 0, false )
+		if #allEnemies > 0 then
+			local minDistance = 10000000
+			for _,enemy in pairs(allEnemies) do
+				local distance = ( thisEntity:GetOrigin() - enemy:GetOrigin() ):Length()
+				if distance < minDistance then
+				  minDistance=distance
+                  self.target=enemy
+				end
+			end
+		end
+
+
+	if self.target and self.target:IsAlive() then
+		self.order =
+		{
+			UnitIndex = thisEntity:entindex(),
+			OrderType = DOTA_UNIT_ORDER_ATTACK_MOVE,
+			Position =  self.target:GetOrigin()
+		}
+	else
+		self.order =
+		{
+			UnitIndex = thisEntity:entindex(),
+			OrderType = DOTA_UNIT_ORDER_STOP
+		}
+	end
+end
+
+ BehaviorNone.Continue=BehaviorNone.Begin
+
+--------------------------------------------------------------------------------------------------------
+
+BehaviorHowl = {}
+
+function BehaviorHowl:Evaluate()
+
+	local desire = 0
+
+	if howlAbility ~= nil and howlAbility:IsCooldownReady() then
+		desire=2
+	end
+	return desire
+end
+
+function BehaviorHowl:Begin()
+	self.endTime = GameRules:GetGameTime() + 1
+
+	ExecuteOrderFromTable({
+		UnitIndex = thisEntity:entindex(),
+		OrderType = DOTA_UNIT_ORDER_CAST_NO_TARGET,
+		AbilityIndex = howlAbility:entindex(),
+	})
+end
+
+BehaviorHowl.Continue = BehaviorHowl.Begin
+
+--------------------------------------------------------------------------------------------------------
+
+AICore.possibleBehaviors = { BehaviorNone, BehaviorHowl}
