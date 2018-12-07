53618794b41caa1dc747ab7597a333bd0b8c2706
oscarlove666 <939178127@qq.com>
2018-12-07 17:02:51 +0800

    12.7

---
+++ game/dota_addons/test3/scripts/vscripts/heroes/hero_doom_bringer/doom_bringer_devour_datadriven.lua
@@ -0,0 +1,19 @@
+LinkLuaModifier( "modifier_doom_bringer_devour_datadriven", "heroes/hero_faceless_void/modifier_doom_bringer_devour_datadriven", LUA_MODIFIER_MOTION_NONE )
+
+
+
+function doom_bringer_devour_datadriven:OnSpellStart()
+
+	local caster = self:GetCaster()
+	local ability = self
+    local target = self:GetCursorTarget()
+    local devour_time = self:GetAbility():GetSpecialValueFor("devour_time")
+
+    local pfx = ParticleManager:CreateParticle("particles/units/heroes/hero_doom_bringer/doom_bringer_devour.vpcf", PATTACH_ABSORIGIN_FOLLOW, caster)
+    ParticleManager:SetParticleControlEnt(pfx, 0, target, PATTACH_POINT_FOLLOW, "attach_origin", target:GetAbsOrigin(), true)
+    ParticleManager:SetParticleControlEnt(pfx, 1, caster, PATTACH_POINT_FOLLOW, "attach_origin", caster:GetAbsOrigin(), true)
+   
+    caster:AddNewModifier(caster, ability, "modifier_doom_bringer_devour_datadriven", {duration = devour_time})
+
+
+end
