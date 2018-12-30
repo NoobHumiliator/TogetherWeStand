5fe5f6697b7b961bc714b7775eb8526cb078d101
oscarlove666 <939178127@qq.com>
2018-12-30 09:21:37 +0800

    12.30

---
+++ game/dota_addons/test3/scripts/vscripts/item_ability/item_ice_dragon_maw.lua
@@ -0,0 +1,10 @@
+item_ice_dragon_maw = class({})
+LinkLuaModifier( "modifier_item_ice_dragon_maw", "item_ability/modifier/modifier_item_ice_dragon_maw", LUA_MODIFIER_MOTION_NONE )
+
+--------------------------------------------------------------------------------
+
+function item_ice_dragon_maw:GetIntrinsicModifierName()
+	return "modifier_item_ice_dragon_maw"
+end
+
+--------------------------------------------------------------------------------
---
+++ game/dota_addons/test3/scripts/vscripts/item_ability/item_winter_embrace.lua
@@ -0,0 +1,10 @@
+item_winter_embrace = class({})
+LinkLuaModifier( "modifier_item_winter_embrace", "item_ability/modifier/modifier_item_winter_embrace", LUA_MODIFIER_MOTION_NONE )
+
+--------------------------------------------------------------------------------
+
+function item_winter_embrace:GetIntrinsicModifierName()
+	return "modifier_item_winter_embrace"
+end
+
+--------------------------------------------------------------------------------
