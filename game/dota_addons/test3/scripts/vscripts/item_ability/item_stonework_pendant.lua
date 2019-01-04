a5d6ba732703d90974c756070c04ae63bb99e269
oscarlove666 <939178127@qq.com>
2019-01-04 12:47:35 +0800

    1.4

---
+++ game/dota_addons/test3/scripts/vscripts/item_ability/item_stonework_pendant.lua
@@ -0,0 +1,10 @@
+item_stonework_pendant = class({})
+LinkLuaModifier( "modifier_item_stonework_pendant", "item_ability/modifier/modifier_item_stonework_pendant", LUA_MODIFIER_MOTION_NONE )
+
+--------------------------------------------------------------------------------
+
+function item_stonework_pendant:GetIntrinsicModifierName()
+	return "modifier_item_stonework_pendant"
+end
+
+--------------------------------------------------------------------------------
