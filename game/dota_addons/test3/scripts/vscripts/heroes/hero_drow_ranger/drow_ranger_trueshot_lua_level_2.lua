426fa9aea22c65e06142cb14ac6e6dcfad9fef28
oscarlove666 <939178127@qq.com>
2019-12-10 19:13:11 +0800

    12.10

---
+++ game/dota_addons/test3/scripts/vscripts/heroes/hero_drow_ranger/drow_ranger_trueshot_lua_level_2.lua
@@ -0,0 +1,18 @@
+drow_ranger_trueshot_lua_level_2 = class({})
+LinkLuaModifier( "modifier_drow_ranger_trueshot_lua_aura_effect", "heroes/hero_drow_ranger/modifier_drow_ranger_trueshot_lua_aura_effect", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_drow_ranger_trueshot_lua_aura", "heroes/hero_drow_ranger/modifier_drow_ranger_trueshot_lua_aura", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_drow_ranger_trueshot_lua_aura_creep", "heroes/hero_drow_ranger/modifier_drow_ranger_trueshot_lua_aura_creep", LUA_MODIFIER_MOTION_NONE )
+
+--------------------------------------------------------------------------------
+
+function drow_ranger_trueshot_lua_level_2:GetIntrinsicModifierName()
+	return "modifier_drow_ranger_trueshot_lua_aura"
+end
+
+--------------------------------------------------------------------------------
+function drow_ranger_trueshot_lua_level_2:OnSpellStart()
+	if IsServer() then
+       self:GetCaster():AddNewModifier(self:GetCaster(), self, "modifier_drow_ranger_trueshot_lua_aura_creep", { duration=self:GetDuration() } )
+	end
+end
+---------------------------------------------------------------------------------
---
+++ game/dota_addons/test3/scripts/vscripts/heroes/hero_drow_ranger/drow_ranger_trueshot_lua_level_3.lua
@@ -0,0 +1,18 @@
+drow_ranger_trueshot_lua_level_3 = class({})
+LinkLuaModifier( "modifier_drow_ranger_trueshot_lua_aura_effect", "heroes/hero_drow_ranger/modifier_drow_ranger_trueshot_lua_aura_effect", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_drow_ranger_trueshot_lua_aura", "heroes/hero_drow_ranger/modifier_drow_ranger_trueshot_lua_aura", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_drow_ranger_trueshot_lua_aura_creep", "heroes/hero_drow_ranger/modifier_drow_ranger_trueshot_lua_aura_creep", LUA_MODIFIER_MOTION_NONE )
+
+--------------------------------------------------------------------------------
+
+function drow_ranger_trueshot_lua_level_3:GetIntrinsicModifierName()
+	return "modifier_drow_ranger_trueshot_lua_aura"
+end
+
+--------------------------------------------------------------------------------
+function drow_ranger_trueshot_lua_level_3:OnSpellStart()
+	if IsServer() then
+       self:GetCaster():AddNewModifier(self:GetCaster(), self, "modifier_drow_ranger_trueshot_lua_aura_creep", { duration=self:GetDuration() } )
+	end
+end
+---------------------------------------------------------------------------------
